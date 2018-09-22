652eeb95ac2167567289ae09a33d88ea5113b15a
Hamsa Lekha Suryadevara <hamsals@internal.synopsys.com>
2018-09-22 11:25:20 +0530

    input_import_module_exploring_std.py

---
+++ input_import_module_exploring_std.py
@@ -0,0 +1,11 @@
+print('impoerted my module')
+
+test='Test string'
+
+def find_index(to_search,target):
+    for i,value in enumerate(to_search):
+        if value==target:
+            return 1
+    return -1
+#cou=['mat','cat','rat']
+#print(find_index(cou,'rat'))
